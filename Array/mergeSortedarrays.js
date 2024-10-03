0e3b52a312fe51b397ad43a7b22546c37d862f9c
nbmdaka <nbmdaka@gmail.com>
2024-10-03 09:34:47 +0500

    Not yet solved problem

--- Array/mergeSortedarrays.js
+++ Array/mergeSortedarrays.js
@@ -1,18 +1,32 @@
 function mergeSortedArrays(arr1, arr2) {
-    const mergedArrays = [];
-    let array1Item = arr1[0];
-    let array2Item = arr2[0];
-    for (let i = 0; i < arr1.length; i ++) {
-        if(arr1[i] < array1Item) {
-            array1Item = arr1[i];
+    const mergedArray = [];
+    let arr1Item = arr1[0];
+    let arr2Item = arr2[0];
+    let i = 1;
+    let j = 1;
+    // Check input
+
+    if(arr1.length === 0) {
+        return arr2;
+    }
+    if(arr2.length === 0) {
+        return arr1;
+    }
+
+    while(arr1Item || arr2Item) {
+        console.log(arr1Item, arr2Item);
+        if(arr2Item === undefined || arr1Item < arr2Item) {
+            mergedArray.push(arr1Item);
+            arr1Item = arr1[i];
+            i++;
+        } else {
+            mergedArray.push(arr2Item);
+            arr2Item = arr2[j];
+            j++;
         }
     }
-    for (let i = 0; i < arr1.length; i ++) {
-        if(arr1[i] < array1Item) {
-            array1Item = arr1[i];
-        }
-    }
-    console.log(array1Item);
+    console.log(mergedArray);
+    return mergedArray;
 }
 
-mergeSortedArrays([0, 3, 4, 31], [4, 6, 30]);
+mergeSortedArrays([0, 3, 4], [4, 6, 30,12]);
