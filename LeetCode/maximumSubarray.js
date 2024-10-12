4f876a5e67ab152cd20bd48684bffb2b8d62f11b
nbmdaka <nbmdaka@gmail.com>
2024-10-12 17:54:10 +0500

    The solved solution Maximum Subarray

--- LeetCode/maximumSubarray.js
+++ LeetCode/maximumSubarray.js
@@ -1,19 +1,21 @@
 var maxSubArray = function(nums) {
-    let localMax = nums[0];
-    let globalMax = nums[0];
+    let currentSum = 0;
+    let maxSubarraySum = nums[0];
 
     for(let i = 0; i < nums.length; i++) {
-        localMax = Math.max(nums[i], localMax + nums[i]);
-        if(localMax > globalMax) {
-            globalMax = localMax
+        currentSum += nums[i];
+        maxSubarraySum = Math.max(maxSubarraySum, currentSum);
+
+        if(currentSum < 0) {
+            currentSum = 0;
         }
     }
-    return globalMax
+
+    return maxSubarraySum;
 };
 
 const nums = [9031,8403,-7093,9614,3293,-1900]
 
 console.log(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]));
 
-// Not solved yet problem maximumSubarray
-// Second day attempt
+// Second Solution in LeetCode;
