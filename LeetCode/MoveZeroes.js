6956b63838242365428ee7b55925e82c5831fa05
nbmdaka <nurdaulet.beketov.2005@gmail.com>
2024-10-18 09:58:53 +0500

    Move Zeroes Task

--- LeetCode/MoveZeroes.js
+++ LeetCode/MoveZeroes.js
@@ -1,4 +1,21 @@
 var moveZeroes = function(nums) {
-
+    let left = 0;
+    let right = 1;
+    while(right < nums.length) {
+        if(nums[right] === 0) {
+            left = 0;
+            right = 1;
+        }
+        if(nums[left] === 0) {
+            let temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+            right++;
+            left++;
+        }
+    }
+    return nums;
 }
+
+console.log(moveZeroes([0,1,0,3,12]));
 
