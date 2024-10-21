1558a6a5a13f4681cb62bea4c47f8df00065d24b
nbmdaka <nurdaulet.beketov.2005@gmail.com>
2024-10-21 19:12:50 +0500

    The fucking solved MoveZero

--- LeetCode/MoveZeroes.js
+++ LeetCode/MoveZeroes.js
@@ -1,28 +1,38 @@
-// var moveZeroes = function(nums) {
-//
-// }
-//
-// console.log(moveZeroes([0,1,0,3,12]));
+var moveZeroes = function(nums) {
+    let left = 0;
 
-var plusOne = function(digits) {
-    let string = ''
-    if(digits[digits.length - 1] < 9){
-        digits[digits.length - 1]+= 1
-        console.log(digits[digits.length - 1])
-        return digits
+    for(let  i = 0; i < nums.length; i++) {
+        if(nums[i] !== 0) {
+            let temp = nums[left];
+            nums[left] = nums[i];
+            nums[i] = temp;
+            left++;
+        }
     }
-    for(let i = 0; i < digits.length; i++) {
-        string += digits[i]
-    }
-    console.log(`Fucking string ${string}`)
-    let sum = parseInt(string) + 1;
-    string = sum.toString()
-    console.log(`Fucking sum ${sum} ` + typeof sum)
-    console.log(`Fucking string ${string} ` + typeof string)
+    return nums;
+}
 
-    for(let i = 0; i < string.length; i++) {
-        digits[i] = parseInt(string[i])
-    }
-    return digits
-};
-console.log(plusOne([1,2,3]));
+console.log(moveZeroes([1,0,0,3,12,0,2]));
+
+// var plusOne = function(digits) {
+//     let string = ''
+//     if(digits[digits.length - 1] < 9){
+//         digits[digits.length - 1]+= 1
+//         console.log(digits[digits.length - 1])
+//         return digits
+//     }
+//     for(let i = 0; i < digits.length; i++) {
+//         string += digits[i]
+//     }
+//     console.log(`Fucking string ${string}`)
+//     let sum = parseInt(string) + 1;
+//     string = sum.toString()
+//     console.log(`Fucking sum ${sum} ` + typeof sum)
+//     console.log(`Fucking string ${string} ` + typeof string)
+//
+//     for(let i = 0; i < string.length; i++) {
+//         digits[i] = parseInt(string[i])
+//     }
+//     return digits
+// };
+// console.log(plusOne([1,2,3]));
--- LeetCode/plusOne.js
+++ LeetCode/plusOne.js
@@ -7,3 +7,26 @@ var plusOne = function(digits) {
     }
 };
 console.log(plusOne([1,2,3]));
+
+// var plusOne = function(digits) {
+//     let string = ''
+//     if(digits[digits.length - 1] < 9){
+//         digits[digits.length - 1]+= 1
+//         console.log(digits[digits.length - 1])
+//         return digits
+//     }
+//     for(let i = 0; i < digits.length; i++) {
+//         string += digits[i]
+//     }
+//     console.log(`Fucking string ${string}`)
+//     let sum = parseInt(string) + 1;
+//     string = sum.toString()
+//     console.log(`Fucking sum ${sum} ` + typeof sum)
+//     console.log(`Fucking string ${string} ` + typeof string)
+//
+//     for(let i = 0; i < string.length; i++) {
+//         digits[i] = parseInt(string[i])
+//     }
+//     return digits
+// };
+// console.log(plusOne([1,2,3]));
